5bd60138c2f426d35185900594046818f850b6c9
Vladimir <bukhtavladimir@gmail.com>
2022-01-06 08:52:27 +0300

    1-st HW props ok
    i done props but dont  layout

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-function Message() {
+
+
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+function Message (props: MessagePropsType) {
     return (
         <div>
+            <section className="chat">
+                <div className="message message--user-1">
+                    <time className="message__time">{props.time}</time>
+                    <figure className="message__author-pic">
+                        <img src={props.avatar} alt={"avatar"}/>
+                    </figure>
+                    <div className="message__text">
+                        <p>{props.message}</p>
+                    </div>
+                    <div>{props.name}</div>
+                </div>
+            </section>
+
+
+
 
         </div>
     )
