73f276d28fd387fed571a69c57f2693a95952daa
Vladimir Bukhta <bukhtavladimir@gmail.com>
2022-01-15 13:31:39 +0300

    refactor 1HW

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 
 
 const messageData = {
-    avatar: 'https://i.pinimg.com/originals/ff/ae/4e/ffae4e028ebe60d38913eb8c8f2255ec.png',
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Vasiliy',
     message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Culpa cum dolores eveniet expedita illo officia placeat quo   reiciendis repudiandae, sed sequi soluta tenetur vero voluptate,    voluptatibus! Cumque cupiditate provident tempore.",
     time: '22:00',
