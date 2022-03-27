7d3585b20f69256c8ba35a203b369bf5c998fb18
Vladimir Bukhta <bukhtavladimir@gmail.com>
2022-03-27 16:31:47 +0300

    HW11 in progress

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import Box from '@material-ui/core/Box'
+import React, {useEffect, useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
@@ -6,31 +7,40 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    type valueObserverType = (value: [number, number]) => void
+    const valueObserver: valueObserverType = (value) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
     return (
         <div>
             <hr/>
-            homeworks 11
+            <h3>Homework 11</h3>
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
+                <Box sx={{width: 400}}>
+                    <SuperDoubleRange
+                        value={[value1, value2]}
+                        onChangeRange={valueObserver}
+
+                        // сделать так чтоб value1 и value2 изменялось
+                    />
+                </Box>
+
+                <span> - {value2}</span>
             </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
             <hr/>
         </div>
     )
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Slider from '@material-ui/core/Slider'
+import React, {ChangeEvent} from 'react'
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
     // min, max, step, disable, ...
 }
+type value1Type = [number, number]
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
@@ -12,11 +14,30 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
         // min, max, step, disable, ...
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const onChangeCallback = (event: ChangeEvent<{}>, value: number | number[]) => {
+        //проверяем, в Пропсах 1 число или массив чисел
+        let value1: value1Type
+        if (typeof(value) === "number") {
+            console.log("value is number")
+        }else if (typeof(value) === "object") {
+            value1 = [value[0],value[1]]
+        }
+
+        // @ts-ignore
+        onChangeRange && onChangeRange(value1)
+
+    }
 
     return (
         <>
-            DoubleRange
+            <Slider
+                // getAriaLabel={() => 'Second slider'}
+                value={value}
+                onChange={onChangeCallback}
+                //valueLabelDisplay="off"
+                // getAriaValueText={"valuetext"}
+            />
         </>
     )
 }
